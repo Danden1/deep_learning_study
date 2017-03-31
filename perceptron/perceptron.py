9fba3feaa64c438b6921f8360bbc541cf2eca43d
Yongha Kwon <goh1211@naver.com>
2017-03-31 22:21:55 +0900

    Update perceptron.py

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -3,7 +3,7 @@ import numpy as np
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5 ,0.5])
-    b = 0.7
+    b = -0.7
     tmp = np.sum(x*w) + b
     if tmp <=0:
         return 0
@@ -23,7 +23,7 @@ def NAND(x1, x2):
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
-    b = 0.2
+    b = -0.2
     tmp = np.sum(x*w) + b
     if tmp <=0:
         return 0
